f79fb75ca88f01b1adaba9a82047df80711c6737
Angelo Carly <angelo.carly@protonmail.com>
2024-03-16 23:11:33 +0100

    Compute an image

--- hexagons/HexagonView.h
+++ hexagons/HexagonView.h
@@ -4,11 +4,16 @@
 #include "burst/Presenter.h"
 #include "burst/Image.h"
 #include "burst/Gui.h"
+#include "vkt/DescriptorSetLayout.h"
 
 #include <glm/glm.hpp>
 
 namespace hex
 {
+    /**
+     * HexagonView, responsible for rendering the hexagons themselves.
+     * Maintains a single image with the hexagons, and an imgui display to view it.
+     */
     class HexagonView
     :
         public burst::Presenter
@@ -24,8 +29,19 @@ namespace hex
             void Present( vk::CommandBuffer inCommandBuffer ) const override;
 
         private:
+            vkt::Device const & mDevice;
             std::shared_ptr< burst::Image > mImage;
             burst::gui::ImageInspector mImageInspector;
+
+            struct PushConstants
+            {
+                int width;
+                int height;
+                int hexSize;
+            };
+
+            vkt::DescriptorSetLayoutsPtr mComputeDescriptorSetLayout;
+            vkt::ComputePipelinePtr mComputePipeline;
     };
 }
 
--- hexagons/Source/HexagonView.cpp
+++ hexagons/Source/HexagonView.cpp
@@ -1,9 +1,13 @@
 #include "hexagons/HexagonView.h"
 
+#include "vkt/Shader.h"
+#include "vkt/ComputePipeline.h"
+
 #include <glm/glm.hpp>
 
 hex::HexagonView::HexagonView( const burst::PresentContext & inContext, glm::ivec2 inResolution )
 :
+    mDevice( inContext.mDevice ),
     mImage( burst::ImageFactory().Create( inContext.mDevice, inResolution, vk::ImageLayout::eGeneral ) ),
     mImageInspector(
         "Hexagon View",
@@ -12,14 +16,38 @@ hex::HexagonView::HexagonView( const burst::PresentContext & inContext, glm::ive
             { mImage->GetSampler(), mImage->GetImageView() },
         },
         [this]( glm::vec2 inPos ){}
-    )
+    ),
+    mComputeDescriptorSetLayout(
+        vkt::DescriptorSetLayoutBuilder( inContext.mDevice )
+            .AddLayoutBinding( 0, vk::DescriptorType::eStorageImage, vk::ShaderStageFlagBits::eCompute )
+            .Build( vk::DescriptorSetLayoutCreateFlagBits::ePushDescriptorKHR )
+   )
 {
+    // Compute shader
+    auto computeShader = vkt::Shader::CreateVkShaderModule( inContext.mDevice, "resources/shaders/hexagons.comp" );
 
+    std::vector< vk::PushConstantRange > pushConstants =
+        {
+            vk::PushConstantRange
+                (
+                    vk::ShaderStageFlagBits::eCompute,
+                    0,
+                    sizeof( PushConstants )
+                )
+        };
+
+    mComputePipeline = vkt::ComputePipelineBuilder( inContext.mDevice )
+        .SetComputeShader( computeShader )
+        .SetDescriptorSetLayouts( mComputeDescriptorSetLayout )
+        .SetPushConstants( pushConstants )
+        .Build();
+
+    inContext.mDevice.GetVkDevice().destroy( computeShader );
 }
 
 hex::HexagonView::~HexagonView()
 {
-
+    mDevice.WaitIdle();
 }
 
 void
@@ -31,7 +59,45 @@ hex::HexagonView::Update( float inDelta )
 void
 hex::HexagonView::Compute( vk::CommandBuffer inCommandBuffer ) const
 {
+    // Begin pipeline
+    mComputePipeline->Bind( inCommandBuffer );
 
+    // Push image descriptor set
+    auto imageInfo = mImage->CreateDescriptorImageInfo();
+    auto theWriteDescriptorSet = vk::WriteDescriptorSet();
+    theWriteDescriptorSet.setDstBinding( 0 );
+    theWriteDescriptorSet.setDstArrayElement( 0 );
+    theWriteDescriptorSet.setDescriptorType( vk::DescriptorType::eStorageImage );
+    theWriteDescriptorSet.setDescriptorCount( 1 );
+    theWriteDescriptorSet.setPImageInfo( & imageInfo );
+
+    mComputePipeline->BindPushDescriptorSet( inCommandBuffer, theWriteDescriptorSet );
+
+    // Push constants
+    static bool sortEven = false;
+    sortEven = !sortEven;
+    PushConstants thePushConstants
+    {
+        int( mImage->GetImage()->GetWidth() ),
+        int( mImage->GetImage()->GetHeight() ),
+        1
+    };
+    mComputePipeline->PushConstants
+    (
+        inCommandBuffer,
+        vk::ShaderStageFlagBits::eCompute,
+        0,
+        sizeof( PushConstants ),
+        & thePushConstants
+    );
+
+    float groupsize = 16;
+    inCommandBuffer.dispatch
+    (
+        ceil( mImage->GetImage()->GetWidth() / groupsize ),
+        ceil( mImage->GetImage()->GetHeight() / groupsize ),
+        1
+    );
 }
 
 void
